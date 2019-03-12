8fbda20c725baf78ede5600e37ec2f93894af4ff
Gita Parmar <gitaparmar83@gmail.com>
2019-03-12 15:07:56 +0530

    React Proj Commit

---
+++ precache-manifest.b0e00bf6566a5b1070f23f8a94f3c54c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "e9347db4ac439b03715c",
+    "url": "/static/js/main.1675fca6.chunk.js"
+  },
+  {
+    "revision": "f708e76424cbd344b707",
+    "url": "/static/js/2.02428bf3.chunk.js"
+  },
+  {
+    "revision": "e9347db4ac439b03715c",
+    "url": "/static/css/main.6d072f56.chunk.css"
+  },
+  {
+    "revision": "f708e76424cbd344b707",
+    "url": "/static/css/2.1557ed5b.chunk.css"
+  },
+  {
+    "revision": "5a9606606e26699a44fa695e253bf1f6",
+    "url": "/index.html"
+  }
+];
